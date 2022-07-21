73a6d885110e84fb9d96303dd13de3ebc45bb8f7
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-21 14:10:34 +0200

    create components and layout static header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div>
+        logo
+    </div>
+    <div>
+        <input type="text" placeholder="Search">
+        <button>Cerca</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

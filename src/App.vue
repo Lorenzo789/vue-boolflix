8d01207c1076962a99b7441948f02510301cb487
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-21 15:29:00 +0200

    emit e get request done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header @search="textToSearch"/>
     <Main />
   </div>
 </template>
@@ -8,13 +8,23 @@
 <script>
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     Header,
     Main
-}
+  },
+  methods: {
+    textToSearch(inputSearch){
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=5d52abd6795445439ccc206e6fdec146&language=it-IT&query=' + inputSearch + '&page=1&include_adult=true')
+      .then((result) => {
+        console.log(result.data);
+        console.log(inputSearch);
+      })
+    }
+  }
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,15 +4,23 @@
         logo
     </div>
     <div>
-        <input type="text" placeholder="Search">
-        <button>Cerca</button>
+        <input type="text" placeholder="Search"
+            v-model="inputSearch">
+
+        <button @click="$emit('search', inputSearch)">
+            Cerca
+        </button>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+            inputSearch: '',
+        }
+    },
 }
 </script>
 
